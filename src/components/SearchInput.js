06a3e6a61bde6546216c769a4e30d8f941295b01
sujan pal <sujanpal79@gmail.com>
2024-12-01 21:11:14 +0530

    Update searchInput Component and debouce part

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const SearchInput = ({search, setSearch}) => {
+    const [inputValue, setInputValue] = useState(search);
+
+    useEffect(()=>{
+        const timer = setTimeout(()=>{
+            setSearch(inputValue);
+        },500)
+        return () => clearTimeout(timer);
+    },[inputValue,setSearch]);
+    const handleChage = (e) =>{
+        setInputValue(e.target.value);
+    }
   return (
-    <div>SearchInput</div>
+    <div>
+        <input
+            type='text'
+            placeholder='Type Something'
+            value={inputValue}
+            onChange={handleChage}
+        />
+    </div>
   )
 }
 

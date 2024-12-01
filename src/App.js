6c7fcfec8303fe61628fe02415ca82af7a51f47d
sujan pal <sujanpal79@gmail.com>
2024-12-01 20:31:24 +0530

    Data fetched from api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { useState, useEffect } from 'react';
 import './App.css';
+import SearchInput from './components/SearchInput';
+import Todolist from './components/Todolist';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(()=>{
+    const fetchData = async() =>{
+      try{
+          const response = await fetch("https://dummyjson.com/todos");
+          if(!response.ok){
+            throw new Error("Faild to fetch data");
+          }
+          const data = await response.json();
+          console.log(data);
+      } catch(error){
+        console.log("Error", error);
+      }
+    }
+    fetchData();
+  },[]);
+  // const filteredData = todos.filter(()=>)
   return (
     <div className="App">
-    <h1>Text Center</h1>
+      <h1>Text Center</h1>
+      <SearchInput search={search} setSearch={setSearch}/>
+      <Todolist  />
     </div>
   );
 }
